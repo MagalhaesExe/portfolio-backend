ce04c191100649d07894aaea095cd659281b2ce9
MagalhaesExe <alexmagalhaesjr15@gmail.com>
2026-08-31 11:52:40 -0300

    refactor: usa logger Winston em vez de console.log na inicialização

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import app from './app.js'
 import { env } from './config/env.js'
+import logger from './config/logger.js'
 
 app.listen(env.PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${env.PORT}`)
+  logger.info(`Servidor rodando na porta ${env.PORT}`)
 })
